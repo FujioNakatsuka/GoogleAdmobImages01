7e7378d672cddfc09a076e62fda22b90f97ed92c
Fujio Nakatsuka <com.nifty.cbm01799>
2020-07-22 21:53:02 +0900

    Initial Commit

---
+++ GoogleAdmobImages01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleAdmobImages01
+//
+//  Created by 中塚富士雄 on 2020/07/22.
+//  Copyright © 2020 中塚富士雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
